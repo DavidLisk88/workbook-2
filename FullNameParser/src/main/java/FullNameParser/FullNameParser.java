df49b18cd8b9dcd8cda232a49bfc030b3023842d
DavidLisk88 <davidperrylisk@gmail.com>
2025-04-16 18:22:43 -0500

    Completed Full name parser exercise

--- FullNameParser/src/main/java/FullNameParser/FullNameParser.java
+++ FullNameParser/src/main/java/FullNameParser/FullNameParser.java
@@ -1,4 +1,92 @@
 package FullNameParser;
 
+import java.util.Scanner;
+
 public class FullNameParser {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        // prompt for first middle and last name
+
+        System.out.println("Please enter your full name.");
+        System.out.println("Format: First, Last OR First, Middle, Last and Suffix (if applicable)");
+
+        // fullName variable will store users first middle and last name.
+        String fullName = userInput.nextLine().trim();
+
+        // Split spaces in between first middle and last name.
+        String[] nameParts = fullName.split(" ");
+
+        // use if statements to print a result based on how the user has input their name.
+        if (nameParts.length == 2){ // this is stating that if the user inputs 2 characters in nameParts (first middle and last).
+            // split the parts of nameParts into first and last name.
+            String firstName = nameParts[0]; // zero is the first part of name parts. which is the users first name.
+            String lastName = nameParts[1];
+            // display the first and last name as the users input and the middle name as none or N/A.
+            System.out.println("First Name: " + firstName);
+            System.out.println("Middle Name: N/A");
+            System.out.println("Last Name: " + lastName);
+        }
+        else if (nameParts.length == 3){ // this is determining if the user input 3 names for nameParts.
+            // redefine how the parts of the name is split
+            String firstName = nameParts[0];
+            String middleName = nameParts[1];
+            String lastName = nameParts[2];
+            // print out name in parts, but this time the output will include middle name.
+            System.out.println("First Name: " + firstName);
+            System.out.println("Middle Name: " + middleName);
+            System.out.println("Last Name: " + lastName);
+        }
+
+        else if (nameParts.length == 4) { // this is determining if the user input 3 names for nameParts.
+            // redefine how the parts of the name is split
+            String firstName = nameParts[0];
+            String middleName = nameParts[1];
+            String lastName = nameParts[2];
+            String suffix = nameParts[3];
+            // print out name in parts, but this time the output will include middle name.
+            System.out.println("First Name: " + firstName);
+            System.out.println("Middle Name: " + middleName);
+            System.out.println("Last Name: " + lastName);
+            System.out.println("Suffix: " + suffix);
+        }
+
+        else {
+            System.out.println("Please enter name in correct format:\nFirst, Last OR First, Middle, Last and Suffix (if applicable)");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
---
+++ FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
